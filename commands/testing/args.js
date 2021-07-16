0ff470ef3db122a559393b5283181beecaa4257d
19tbusst <19tbusst@wakatipu.school.nz>
2021-07-16 16:12:00 +1200

    Update args.js
    - addon to previous commit, aka I'm stupid

--- commands/testing/args.js
+++ commands/testing/args.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'Args test command.',
     args: true,
     usage: "<args>",
+    permissions: 'ADMINISTRATOR',
     execute(message, args) {
         message.channel.send(`Your arguments are: ${args}`);
     }
