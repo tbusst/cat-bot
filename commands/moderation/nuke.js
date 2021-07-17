dcb8adaa1c50ec2b9f5521b945cbc12e922befe1
19tbusst <19tbusst@wakatipu.school.nz>
2021-07-17 22:26:06 +1200

    Testing

--- commands/moderation/nuke.js
+++ commands/moderation/nuke.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'nuke',
-    description: 'Deletes an specified amount of messages.',
+    description: 'Deletes a specified amount of messages.',
     args: true,
     usage: '<amount>',
     aliases: ['purge', 'bulkdelete'],
--- commands/utility/help.js
+++ commands/utility/help.js
@@ -7,6 +7,8 @@ module.exports = {
         const data = [];
         const { commands } = message.client;
 
+        console.log(message.channel.id);
+
         if (!args.length) {
             data.push('Here\'s a list of my available commands:');
             data.push(commands.map(command => command.name).join(', '));
