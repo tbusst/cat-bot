f1be504208248bff09ab7b002b3c359d6a591152
19tbusst <19tbusst@wakatipu.school.nz>
2021-07-22 14:15:16 +1200

    Coin Flip

---
+++ commands/fun/coinFlip.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name = 'coinflip',
+    description = 'Flips a coin.',
+    aliases = ['coin', 'flip'],
+    args = false,
+    execute(message) {
+        let side;
+
+        if (Math.random() > 0.5) side = 'Heads' 
+        else side = 'Tails'
+
+        message.channel.send(`${message.author} Fliped a coin and got ${side}!`);
+    }
+}
